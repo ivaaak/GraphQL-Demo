ed8b53d907e7584fc9dc938ace7437bc1287a347
ivaaak <43663336+ivaaak@users.noreply.github.com>
2022-04-20 10:51:10 +0300

    added values

--- Services/DataAccess.cs
+++ Services/DataAccess.cs
@@ -9,7 +9,11 @@ namespace GQL.Demo
             new Product(1, "Laptop", 20),
             new Product(2, "Mouse", 30),
             new Product(3, "Keyboard", 10),
-            new Product(4, "Monitor", 40)
+            new Product(4, "Monitor", 40),
+            new Product(5, "Printer", 20),
+            new Product(6, "Headphones", 20),
+            new Product(7, "Amp", 10),
+            new Product(8, "PSU", 20),
         };
 
         public IList<Product> Get() => products;
