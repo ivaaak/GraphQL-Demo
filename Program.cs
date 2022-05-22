d698cea09749bb76154abcadc1f95b822c832b3b
Ivo <43663336+ivaaak@users.noreply.github.com>
2022-05-22 11:48:05 +0300

    formatting

--- Program.cs
+++ Program.cs
@@ -1,9 +1,9 @@
 global using GraphQL;
 global using GraphQL.Types;
+
 using GQL.Demo.Interfaces;
 using GQL.Demo.Services;
 using Microsoft.AspNetCore.Mvc;
-
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,20 +12,18 @@ builder.Services.AddApiServices();
 
 var app = builder.Build();
 
-
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-app.UseHttpsRedirection();//.UseAuthorization();
+app.UseHttpsRedirection();//.UseAuthorization(); no auth added currently
 
 app.MapGet("/api/products", ([FromServices] IProductProvider productProvider) =>
 {
     return productProvider.GetProducts();
 })
 .WithName("GetProducts");
-
 
 app.UseGraphQLAltair();
 app.UseGraphQL<ISchema>();
